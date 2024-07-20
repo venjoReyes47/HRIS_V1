c4e659d030633a31324b7b4d0ea0ad285860ecc3
Venjo Reyes <reyesvenjoyce22@gmail.com>
2024-07-20 16:08:52 +0800

    Commit Build

---
+++ build/precache-manifest.5356fc103d8e33e0ecc12dabfcb4bd90.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c215766ae6e432490ad84f3d66aad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5445d74e64dc741b531c",
+    "url": "/static/css/2.93e3fb29.chunk.css"
+  },
+  {
+    "revision": "6af8caf61181c553c363",
+    "url": "/static/css/main.3357adca.chunk.css"
+  },
+  {
+    "revision": "5445d74e64dc741b531c",
+    "url": "/static/js/2.20f95ae4.chunk.js"
+  },
+  {
+    "revision": "6af8caf61181c553c363",
+    "url": "/static/js/main.6f1a8811.chunk.js"
+  },
+  {
+    "revision": "704cb5a44dd6dea5a15b",
+    "url": "/static/js/runtime-main.5cf3bc4a.js"
+  },
+  {
+    "revision": "35d544eaaa4cf3c6355866280d53ba73",
+    "url": "/static/media/Flaticon.35d544ea.eot"
+  },
+  {
+    "revision": "3e4331ee31764c999add7e0b048c4ba3",
+    "url": "/static/media/Flaticon.3e4331ee.ttf"
+  },
+  {
+    "revision": "500d63cca8c534718b1d202b2076d218",
+    "url": "/static/media/Flaticon.500d63cc.svg"
+  },
+  {
+    "revision": "5be3e43c13c3eb021d15e6682d098d4c",
+    "url": "/static/media/Flaticon.5be3e43c.woff"
+  },
+  {
+    "revision": "29586ff0f963f4d1fdfc182822b8b27a",
+    "url": "/static/media/Flaticon2.29586ff0.eot"
+  },
+  {
+    "revision": "4248a89254d2c6c396d85e378184c54f",
+    "url": "/static/media/Flaticon2.4248a892.svg"
+  },
+  {
+    "revision": "b242ac810bd8cccaa03abc2128b7c3c3",
+    "url": "/static/media/Flaticon2.b242ac81.woff"
+  },
+  {
+    "revision": "eafcbac04cdb0a39fe38a36ebd786290",
+    "url": "/static/media/Flaticon2.eafcbac0.ttf"
+  },
+  {
+    "revision": "4b89604081d15ff4f70c0dbae7feca2f",
+    "url": "/static/media/Ki.4b896040.ttf"
+  },
+  {
+    "revision": "9b3ea13067aa8273a3ae2b3241a6f6b3",
+    "url": "/static/media/Ki.9b3ea130.svg"
+  },
+  {
+    "revision": "9e253c785967880c681cbd59237ea615",
+    "url": "/static/media/Ki.9e253c78.eot"
+  },
+  {
+    "revision": "a64552c2da17518608c3b54c18fb3fa0",
+    "url": "/static/media/Ki.a64552c2.woff"
+  },
+  {
+    "revision": "2ba5903abc623a15fb464d82d818b093",
+    "url": "/static/media/SchoolAttendanceLogo.2ba5903a.png"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "7b6f52073603291eb60040b7d13b4054",
+    "url": "/static/media/faithColleges.7b6f5207.png"
+  },
+  {
+    "revision": "db98b6a44e59898364350716e7d9cbf1",
+    "url": "/static/media/person.db98b6a4.jpg"
+  },
+  {
+    "revision": "60e5857089e98edd838074c264d6c951",
+    "url": "/static/media/socicon.60e58570.eot"
+  },
+  {
+    "revision": "944f06f5f65ef84a3a36e6c1c2d4b7ad",
+    "url": "/static/media/socicon.944f06f5.woff"
+  },
+  {
+    "revision": "9a64ef938f9e55a70a4defc6ac9790bf",
+    "url": "/static/media/socicon.9a64ef93.ttf"
+  },
+  {
+    "revision": "a35b65744f557fab5424e99bb6d4e980",
+    "url": "/static/media/socicon.a35b6574.svg"
+  }
+]);
